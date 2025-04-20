44e8cdfafe65ecf6268e617f1b80a5c2c6e7edce
Ардак Чапаев <31521003+ardakchapaev@users.noreply.github.com>
2025-04-21 05:31:47 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,5 @@ app = Flask(__name__, template_folder="templates", static_folder="static")
 def index():
     return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000)
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello from BaiMuras"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
