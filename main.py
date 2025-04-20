2ff4ecca36cd542012b79757187cb2fade5dd001
Ардак Чапаев <31521003+ardakchapaev@users.noreply.github.com>
2025-04-21 05:26:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ def index():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3000)
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from BaiMuras"
